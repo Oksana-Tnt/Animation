3d32190fd567badb4578fead1091b51e534f5128
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-08-14 20:28:41 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*let element = document.querySelector("#svg__lettersG__R1");*/
-let element = document.querySelector("#svg__lettersG__R6");
+let element = document.querySelector("#svg__lettersGF__R3");
 
 console.log(element.getTotalLength());
 
